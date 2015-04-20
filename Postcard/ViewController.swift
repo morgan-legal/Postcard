7988b68e01bfacd38d46c92c649687c821b291b9
morgan-legal <legal.morgan@gmail.com>
2015-04-20 08:21:01 -0700

    added comments to test commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // test GIT HUB
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
